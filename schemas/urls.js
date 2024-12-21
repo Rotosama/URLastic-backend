ca7c2af96f89d1bc961ce7a819b24fef27818826
Nacho Ruiz <n.ruizgarcia@hotmail.com>
2024-12-21 11:39:08 +0100

    timestamps added to urlSchema

--- schemas/urls.js
+++ schemas/urls.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const urlSchema = new Schema({
-    originalUrl: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    shortenUrl: {
-        type: String,
-        trim: true,
-    },
-    user: String,
-});
+const urlSchema = new Schema(
+	{
+		originalUrl: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		shortenUrl: {
+			type: String,
+			trim: true,
+		},
+		user: String,
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model("url", urlSchema);
